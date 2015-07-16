da936651fac54de7380f105680b05f332d46baf7
sergey moldachev <sergeymold@gmail.com>
2015-07-16 13:39:29 +0500

    Refactor component

--- install/components/citfact/replaceurl/component.php
+++ install/components/citfact/replaceurl/component.php
@@ -9,17 +9,7 @@ Loader::includeModule('citfact.replaceurl');
 
 $PROP_CODE = Config\Option::get("citfact.replaceurl", "PROPERTY_CODE", "MAIN_SECTION");
 $IBLOCK_ID = Config\Option::get("citfact.replaceurl", "IBLOCK_ID", "");
-$app = Application::getInstance();
-$currentUri = $app->getContext()->getServer()->getRequestUri();
+$replaceDetailURL = new Replaceurl\Detail($arParams["~array_modifier"],$PROP_CODE,$IBLOCK_ID);
+$replaceDetailURL->updateURL();
 
-if(!empty($arParams["~array_modifier"]["ITEMS"])){
-    $replaceSectURL = new Replaceurl\Section($arParams["~array_modifier"]["ITEMS"],$PROP_CODE,$IBLOCK_ID);
-    $arParams["~array_modifier"]["ITEMS"] = $replaceSectURL->updateURL();
 
-}else{
-    $replaceDetailURL = new Replaceurl\Detail($arParams["~array_modifier"],$PROP_CODE,$IBLOCK_ID);
-    $arParams["~array_modifier"] = $replaceDetailURL->updateURL();
-    $replaceDetailURL->localRedirect($currentUri);
-}
-
-return $arParams["~array_modifier"];
